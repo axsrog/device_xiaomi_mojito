aeeeda27b0928d5c9bc9cd52f5c621f7fcf07c57
Arefin <ahmedarefinsajjad@gmail.com>
2023-06-05 13:37:25 +0600

    mojito: lmodroid bringup

--- lmodroid_mojito.mk
+++ lmodroid_mojito.mk
@@ -14,19 +14,17 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some common PixelExperience stuff
-TARGET_USES_AOSP_RECOVERY := true
+# Inherit some common LineageOS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_mojito
+PRODUCT_NAME := lmodroid_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
