08261c507b428a98613ca2e4239d8512751627cd
Ahmed Arefin <ahmedarefinsajjad@gmail.com>
2023-07-06 21:08:52 +0600

    sunny: Zephyrus bringup
    Signed-off-by: Ahmed Arefin <ahmedarefinsajjad@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/statix_sunny.mk
+    $(LOCAL_DIR)/sunny.mk
--- sunny.mk
+++ sunny.mk
@@ -11,13 +11,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sunny device
 $(call inherit-product, device/xiaomi/sunny/device.mk)
 
-# Inherit some common StatiX stuff.
-$(call inherit-product, vendor/statix/config/common.mk)
-$(call inherit-product, vendor/statix/config/gsm.mk)
+# Inherit some common Zephyrus stuff.
+$(call inherit-product, vendor/zephyrus/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := statix_sunny
+PRODUCT_NAME := sunny
 PRODUCT_DEVICE := sunny
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := M2101K7AG
@@ -25,7 +24,5 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=sunny
-
-BUILD_FINGERPRINT := "Redmi/sunny_global/sunny:12/RKQ1.210614.002/V13.0.11.0.SKGMIXM:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
